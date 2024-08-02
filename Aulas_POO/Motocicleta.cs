5f7b6add5605c01c0f963720f097a6779e91d7cd
cris_mila <cristianmila02@gmail.com>
2024-08-02 18:55:26 -0300

    Exercio E1

---
+++ Aulas_POO/Motocicleta.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Aulas_POO
+{
+    public class Motocicleta
+    {
+        // Atributos
+        private string _marca;
+        private string _modelo;
+
+        // Propriedades
+        public string Marca
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        }
+        public string Modelo
+        {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+
+        // Métodos
+        public void Acelerar()
+        {
+            Console.WriteLine($"{_marca} {_modelo} está acelerando...");
+        }
+
+        // Sobrecarga de métodos
+        public void Frenar()
+        {
+            Console.WriteLine($"{_marca} {_modelo} está frenando...");
+        }
+        public bool Frenar(string tipo)
+        {
+            Console.WriteLine($"{_marca} {_modelo} está frenando com {tipo}...");
+            return true;
+        }
+
+        // Método sobrecarga
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine($"{_marca} {_modelo} está emitindo som do motor...");
+        }
+
+        // Método sobrescrita
+        public virtual void Abastecer()
+        {
+            Console.WriteLine($"{_marca} {_modelo} está sendo abastecida...");
+        }
+
+        // Construtor
+        public Motocicleta(string marca, string modelo)
+        {
+            _marca = marca;
+            _modelo = modelo;
+        }
+
+        // Classe filha (subclasse)
+        public class Esportiva : Motocicleta
+        {
+            private int _cilindradas;
+            private string _cor;
+
+            public int Cilindradas
+            {
+                get { return _cilindradas; }
+                set { _cilindradas = value; }
+            }
+
+            public string Cor
+            {
+                get { return _cor; }
+                set { _cor = value; }
+            }
+
+            public void Empinar()
+            {
+                Console.WriteLine($"{Marca} {Modelo} está empinando...");
+            }
+
+            // Construtor da subclasse
+            public Esportiva(string marca, string modelo, int cilindradas, string cor)
+                : base(marca, modelo)
+            {
+                _cilindradas = cilindradas;
+                _cor = cor;
+            }
+
+            // Sobrescrita de método
+            public override void EmitirSom()
+            {
+                Console.WriteLine($"{Marca} {Modelo} está emitindo um som esportivo...");
+            }
+            public override void Abastecer()
+            {
+                Console.WriteLine($"{Marca} {Modelo} está sendo abastecida com combustível premium...");
+            }
+        }
+    }
+}
---
+++ Aulas_POO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Aulas_POO;
+
+var motocicleta = new Motocicleta("Honda", "CB500F");
+motocicleta.Acelerar();
+motocicleta.Frenar();
+motocicleta.Abastecer();
+motocicleta.EmitirSom();
+Console.WriteLine(motocicleta);
+
+Motocicleta.Esportiva minhaEsportiva = new Motocicleta.Esportiva("Yamaha", "YZF-R1", 998, "Azul");
+minhaEsportiva.Acelerar();
+minhaEsportiva.Frenar();
+minhaEsportiva.Abastecer();
+minhaEsportiva.EmitirSom();
+minhaEsportiva.Empinar();
+Console.WriteLine(minhaEsportiva);
